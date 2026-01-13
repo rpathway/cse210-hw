76c8ec51ed13eb0101084e76a004b78a318ddfd0
rpathway <robbieyamashita1@byupathway.edu>
2026-01-13 15:34:20 -0500

    Added week02 Activity - Abstraction (Resume)

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,19 @@
+using System.Configuration.Assemblies;
+using System.IO.Pipes;
+
+public class Resume
+{
+  public string _name = "";
+  public List<Job> _jobs = new List<Job>();
+
+  public void DisplayResumeDetails()
+  {
+    Console.WriteLine($"Name: {_name}");
+    Console.WriteLine($"Jobs:");
+
+    foreach (Job jobHistory in _jobs)
+    {
+      jobHistory.DisplayJobDetails();
+    }
+  }
+}
